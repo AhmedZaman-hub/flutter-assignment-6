9f2fd5faeff5b8bdbf70ef3dac73ab9212dafdcf
Ahmed Zaman <zaman994ahmed@gmail.com>
2024-03-30 21:52:01 +0500

    Question 8 script

---
+++ Question 8.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<Map<String, dynamic>> usersEligibility = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
+  ];
+
+  usersEligibility.removeWhere((element) => false == element['eligible']);
+
+  usersEligibility.forEach((user) {
+    print('${user['name']}: eligible - ${user['eligible']}');
+  });
+}
